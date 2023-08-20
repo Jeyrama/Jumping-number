b3307733a6dca502bf20cba4ff89d0ce360276aa
Jey <126204426+Jeyrama@users.noreply.github.com>
2023-08-20 00:00:41 -0700

    Update Jumping-number.js

--- Jumping-number.js
+++ Jumping-number.js
@@ -8,3 +8,13 @@ Given a number, Find if it is Jumping or not.
 
 
 // Solution
+
+function jumpingNumber(n){
+  let arr = n.toString().split('')
+  for(i=0; i < arr.length-1; i++){
+    if(Math.abs(arr[i] - arr[i+1]) !== 1 ){
+      return 'Not!!'
+    }
+  }
+  return 'Jumping!!'
+}
